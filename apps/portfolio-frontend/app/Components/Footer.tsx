9741ccaca800ce18c195c50b6f34a3b699801960
akshxdevs <akshxdevs@gmail.com>
2025-09-09 17:42:30 +0530

    added footer

--- apps/portfolio-frontend/app/Components/Footer.tsx
+++ apps/portfolio-frontend/app/Components/Footer.tsx
@@ -1,6 +1,7 @@
 export const Footer = () => {
     return (
-        <div>
+        <div className="text-center py-8 text-slate-400 text-sm">
+            © 2025 Akshxdevs. All rights reserved.
         </div>
     );
 };
