c61d9bcf8f6670189a7639ddf428279dfd2cfaa1
akshxdevs <akshxdevs@gmail.com>
2025-09-10 08:35:11 +0530

    blog added

--- apps/portfolio-frontend/app/Components/BlogSection.tsx
+++ apps/portfolio-frontend/app/Components/BlogSection.tsx
@@ -2,7 +2,12 @@ export const BlogSection = () => {
   return (
     <div className="max-w-5xl mx-auto py-5">
       <div className="px-32">
-        
+        <h1 className="text-2xl text-slate-50 font-semibold">
+          recent blogs
+        </h1>
+        <div>
+          {/* {blog's from backend fetched and displayed here} */}
+        </div>
       </div>
     </div>
   );
