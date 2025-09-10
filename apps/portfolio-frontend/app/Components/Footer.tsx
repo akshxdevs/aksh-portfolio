7d0872c9a4022f1f35e4aca99a8396622a2ff513
akshxdevs <akshxdevs@gmail.com>
2025-09-10 09:25:20 +0530

    changes made

--- apps/portfolio-frontend/app/Components/Footer.tsx
+++ apps/portfolio-frontend/app/Components/Footer.tsx
@@ -1,7 +1,12 @@
 export const Footer = () => {
-    return (
-        <div className="text-center py-8 text-slate-400 text-sm">
-            © 2025 Akshxdevs. All rights reserved.
-        </div>
-    );
+  return (
+    <div className="text-center py-8 text-slate-400 text-sm">
+      <div className={`text-slate-400 text-sm`}>
+        Built with passion & precision by
+        <span className="font-bold px-1"> @akshxdevs</span>— where innovation
+        meets execution. 
+      </div>
+      <p>© 2025 Akshxdevs. All rights reserved.</p>
+    </div>
+  );
 };
