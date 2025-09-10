e2b9c2d525dd916d29f6089b2d74269942e78985
akshxdevs <akshxdevs@gmail.com>
2025-09-10 08:44:22 +0530

    added contact option

--- apps/portfolio-frontend/app/Components/Contact.tsx
+++ apps/portfolio-frontend/app/Components/Contact.tsx
@@ -1,9 +1,20 @@
 export const Contact = () => {
-    return (
+  return (
     <div className="max-w-5xl mx-auto py-5">
-      <div className="px-32">
-        
-      </div>
+      <div className="flex flex-col gap-1 px-32">
+        <h1 className="text-2xl text-slate-50 font-semibold">
+          Hire Me, Meet Me, DM Me
+        </h1>
+        <p className="text-md text-slate-300">
+          reach out for work, collabs, or just a chat on x. book a slot, or drop
+          straight in.
+        </p>
+        <div className="flex gap-3 mt-2">
+          <button className="border rounded-lg p-2">Hire me</button>
+          <button className="border rounded-lg p-2">Book a meet</button>
+          <button className="border rounded-lg p-2">Get in touch</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
