2be24d41c4d75857efb7ef011c5e5f7f79a665ec
akshxdevs <akshxdevs@gmail.com>
2025-09-10 10:51:36 +0530

    added changes

--- apps/portfolio-frontend/app/Components/Footer.tsx
+++ apps/portfolio-frontend/app/Components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="text-center pt-8 text-slate-400 text-sm">
+    <div className="text-center pt-8 pb-20 text-slate-400 text-sm">
       <div className={`text-slate-400 text-sm`}>
         Built with passion & precision by
         <span className="font-bold px-1"> @akshxdevs</span>— where innovation
