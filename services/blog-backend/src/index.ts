b85f9541596f9c9ee75312365d60e03fe82e8d04
akshxdevs <akshxdevs@gmail.com>
2025-09-11 14:23:37 +0530

    changes made in the blog be

--- services/blog-backend/src/index.ts
+++ services/blog-backend/src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config";
 import { blogRouter } from "./routes/blog";
+import { userRouter } from "./routes/user";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/blog",blogRouter)
+app.use("/api/v1/user",userRouter)
 
 app.listen(PORT,()=>{
     console.log("Server running on port 3000");
