66ece14ce7eaeba2332a7afbc6da53cd7f65886b
akshxdevs <akshxdevs@gmail.com>
2025-09-12 23:15:08 +0530

    update: added animated buttons

--- apps/portfolio-frontend/app/Components/Contact.tsx
+++ apps/portfolio-frontend/app/Components/Contact.tsx
@@ -14,9 +14,28 @@ export const Contact = () => {
           reach out for work, collabs, or just a chat on x. book a slot, or drop
           straight in.
         </p>
-        <div className="flex gap-3 mt-2">
-          <button className="border rounded-lg p-2">Hire me</button>
-          <button className="border rounded-lg p-2">Get in touch</button>
+        <div className="flex space-x-4 mt-4">
+          <button className="relative rounded-lg p-2 overflow-hidden group ">
+              <div className="absolute inset-0 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300" style={{animation: 'gradient-rotate 0.5s linear infinite', animationPlayState: 'running'}}></div>
+              <div className="absolute inset-[1.5px] rounded-lg bg-zinc-800 transition-colors duration-300"></div>
+              <a href="" className="relative flex items-center gap-2 z-10">
+                <img
+                  width="20"
+                  height="20"
+                  src="https://img.icons8.com/ios-filled/50/FFFFFF/about-me.png"
+                  alt="about-me"
+                  className="transition-all duration-300 group-hover:brightness-110"
+                />
+                <span className="text-gray-800 dark:text-white transition-colors duration-300">Hire me</span>
+              </a>
+            </button>
+            <button className="relative rounded-lg p-2 overflow-hidden group">
+              <div className="absolute inset-0 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300" style={{animation: 'twitter-gradient 0.5s linear infinite', animationPlayState: 'running'}}></div>
+              <div className="absolute inset-[1px] rounded-lg bg-zinc-800 transition-colors duration-300"></div>
+              <a href={`https://x.com/akshxdevs`} target="_blank" rel="noopener noreferre" className="relative flex items-center gap-1 z-10">
+                <span className="text-slate-200 transition-colors duration-300">Get in touch</span>
+              </a>
+            </button>
         </div>
       </div>
     </div>
