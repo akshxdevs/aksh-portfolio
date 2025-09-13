0d200d62fde1751b9f9d6292d3cfbb63818be76f
akshxdevs <akshxdevs@gmail.com>
2025-09-13 10:54:45 +0530

    update: theme func added

--- apps/portfolio-frontend/app/Components/Footer.tsx
+++ apps/portfolio-frontend/app/Components/Footer.tsx
@@ -1,12 +1,15 @@
+'use client';
+import { useTheme } from "../contexts/ThemeContext";
 export const Footer = () => {
+  const {theme} = useTheme();
   return (
     <div className="text-center pt-8 pb-20 text-slate-400 text-sm">
-      <div className={`text-slate-400 text-sm`}>
+      <div className={`${theme === "dark" ? "text-slate-400" : "text-slate-900"} text-sm`}>
         Built with passion & precision by
         <span className="font-bold px-1"> @akshxdevs</span>— where innovation
         meets execution. 
       </div>
-      <p>© 2025 Akshxdevs. All rights reserved.</p>
+      <p className={`${theme === "dark" ? "text-slate-400" : "text-slate-900"} text-sm`}>© 2025 Akshxdevs. All rights reserved.</p>
     </div>
   );
 };
