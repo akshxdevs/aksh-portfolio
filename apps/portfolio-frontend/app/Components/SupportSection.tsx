12f7c3ff17fb319ed200ffd01634d6f39318aa05
akshxdevs <akshxdevs@gmail.com>
2025-09-13 22:07:28 +0530

    update: light theme icons added

--- apps/portfolio-frontend/app/Components/SupportSection.tsx
+++ apps/portfolio-frontend/app/Components/SupportSection.tsx
@@ -1,13 +1,19 @@
-'use client';
+"use client";
 import { useTheme } from "../contexts/ThemeContext";
 
 export const SupportSection = () => {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   return (
     <div className="max-w-5xl mx-auto py-5">
       <div className="flex flex-col gap-2 px-32">
-        <h1 className={`text-2xl ${theme === "dark" ? "text-slate-50" : "text-slate-900"} font-semibold`}>Support Me</h1>
-        <p className={`text-md ${theme === "dark" ? "text-zinc-200" : "text-slate-900"}`}>
+        <h1
+          className={`text-2xl ${theme === "dark" ? "text-slate-50" : "text-slate-900"} font-semibold`}
+        >
+          Support Me
+        </h1>
+        <p
+          className={`text-md ${theme === "dark" ? "text-zinc-200" : "text-slate-900"}`}
+        >
           if you vibe with my work, projects, or content, consider supporting
           me. every bit of support helps me keep creating, improving, and
           sharing more.
@@ -40,14 +46,27 @@ export const SupportSection = () => {
             />
             Solana
           </button>
-          <button className="flex items-center gap-1 border border-zinc-600 rounded-lg p-2">
-            <img
-              width="30"
-              height="30"
-              src="https://img.icons8.com/ios-filled/50/FFFFFF/bhim-upi.png"
-              alt="bhim-upi"
-            />
-            UPI
+          <button className="border border-zinc-600 rounded-lg p-2">
+            <div className="flex items-center gap-1 group">
+              {theme === "dark" ? (
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/ios-filled/50/FFFFFF/bhim-upi.png"
+                  alt="bhim-upi"
+                  className="group-hover:animate-bounce"
+                />
+              ) : (
+                  <img
+                    width="30"
+                    height="30"
+                    src="https://img.icons8.com/ios-filled/50/bhim-upi.png"
+                    alt="bhim-upi"
+                    className="group-hover:animate-bounce"
+                  />
+              )}
+              <p className="animate-none">UPI</p>
+            </div>
           </button>
         </div>
       </div>
