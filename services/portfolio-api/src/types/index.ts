e11519e1097bd6200cd7bc652e476f91ee4b53d3
akshxdevs <akshxdevs@gmail.com>
2025-09-15 21:03:11 +0530

    Add debug endpoint to show database connection info

--- services/portfolio-api/src/types/index.ts
+++ services/portfolio-api/src/types/index.ts
@@ -2,13 +2,13 @@ import { Status } from "@prisma/client";
 import { z } from "zod";
 
 export const projectSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  intro: z.string(),
-  githubLink: z.string().trim(),
+  title: z.string().trim().max(50).min(10),
+  description: z.string().trim().max(100).min(30),
+  intro: z.string().trim().max(30).min(10),
+  githubLink: z.string().trim().url(),
   category: z.string().trim(),
   status: z.nativeEnum(Status),
-  imgUrl: z.string().trim(),
-  webUrl: z.string().trim(),
+  imgUrl: z.string().trim().url(),
+  webUrl: z.string().trim().url(),
   techStack: z.array(z.string()),
 });
