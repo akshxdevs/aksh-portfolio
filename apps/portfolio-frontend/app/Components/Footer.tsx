88711e5866904bb3da3bb3dfa9cbf18a1c80f2a9
akshxdevs <akshxdevs@gmail.com>
2025-09-15 14:05:19 +0530

    udpate: made styling changes

--- apps/portfolio-frontend/app/Components/Footer.tsx
+++ apps/portfolio-frontend/app/Components/Footer.tsx
@@ -3,13 +3,13 @@ import { useTheme } from "../contexts/ThemeContext";
 export const Footer = () => {
   const {theme} = useTheme();
   return (
-    <div className="text-center pt-8 pb-20 text-slate-400 text-sm">
-      <div className={`${theme === "dark" ? "text-slate-400" : "text-slate-900"} text-sm`}>
+    <div className="text-center pt-8 pb-20 text-sm">
+      <div className={`${theme === "dark" ? "text-zinc-100" : "text-zinc-900"} text-sm`}>
         Built with passion & precision by
         <span className="font-bold px-1"> @akshxdevs</span>— where innovation
         meets execution. 
       </div>
-      <p className={`${theme === "dark" ? "text-slate-400" : "text-slate-900"} text-sm`}>© 2025 Akshxdevs. All rights reserved.</p>
+      <p className={`${theme === "dark" ? "text-zinc-200" : "text-zinc-800"} text-sm`}>© 2025 Akshxdevs. All rights reserved.</p>
     </div>
   );
 };
