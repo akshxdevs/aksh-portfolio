8b10ace2af8010f44b365725c18e9224297165e0
akshxdevs <akshxdevs@gmail.com>
2025-09-16 10:11:06 +0530

    update: added responsive set to mobile size

--- apps/portfolio-frontend/app/Components/Education.tsx
+++ apps/portfolio-frontend/app/Components/Education.tsx
@@ -5,9 +5,9 @@ export const Education = () => {
   const {theme} = useTheme();
   return (
     <div className="max-w-5xl mx-auto py-5">
-      <div className="px-32">
+      <div className="px-5 sm:px-32">
         <h1 className={`text-3xl ${theme === "dark" ? "text-slate-50" : "text-slate-900"} font-bold`}>Education</h1>
-        <div className="flex justify-between mt-4">
+        <div className="flex flex-col sm:flex-row justify-between gap-3 mt-4">
           <div className="flex items-center gap-2">
             <img
               src="https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Anna_University_Logo.svg/250px-Anna_University_Logo.svg.png"
@@ -15,12 +15,12 @@ export const Education = () => {
               className="h-16 w-16 border border-zinc-800 rounded-full"
             />
             <div>
-              <h1 className={`text-xl ${theme === "dark" ? "text-slate-100" : "text-slate-900"} font-bold`}>Electronics and Communication Engineering</h1>
+              <h1 className={`text-lg sm:text-xl ${theme === "dark" ? "text-slate-100" : "text-slate-900"} font-bold`}>Electronics and Communication Engineering</h1>
               <p>Anna University</p>
             </div>
           </div>
-          <div className="flex justify-center items-center">
-            <p className="text-md bg-zinc-800 text-slate-200 px-2 py-1 rounded-xl">2021 - 2025</p>
+          <div className="mx-8 w-1/2 sm:w-full flex justify-center items-center">
+            <p className="text-sm sm:text-md bg-zinc-800 text-slate-200 px-2 py-1 rounded-xl">2021 - 2025</p>
           </div>
         </div>
       </div>
