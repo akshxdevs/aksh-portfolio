284598e36d2a4a0e897179d55de538aaa0aaedca
akshxdevs <akshxdevs@gmail.com>
2025-09-22 14:40:54 +0530

    update: changes made in blog backend

--- services/blog-backend/src/index.ts
+++ services/blog-backend/src/index.ts
@@ -2,16 +2,13 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config";
 import { blogRouter } from "./routes/blog";
-import { userRouter } from "./routes/user";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/blog",blogRouter)
-app.use("/api/v1/user",userRouter)
+app.use("/api/v1/blog", blogRouter);
 
-app.listen(PORT,()=>{
-    console.log("Server running on port 3000");
-    
-})
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
