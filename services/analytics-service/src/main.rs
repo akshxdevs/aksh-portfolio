81049278ac51a7baaeb704d28f3caa8f99d67ff1
akshxdevs <akshxdevs@gmail.com>
2025-09-22 19:46:07 +0530

    update: added store/api to the main.rs

--- services/analytics-service/src/main.rs
+++ services/analytics-service/src/main.rs
@@ -1,18 +1,21 @@
+use axum::{Router};
 use futures_util::StreamExt;
 use services::{kafka::{init_consumer, init_producer}, redis::init_redis_pool, ws};
 use tokio::{sync::broadcast};
-use rdkafka::{consumer::{CommitMode, Consumer, StreamConsumer}, Message};
+use rdkafka::{consumer::{CommitMode, Consumer, StreamConsumer}};
+use rdkafka::Message;
 use std::{net::SocketAddr, sync::Arc};
 use log::{info};
 
 mod services;
-
+use store::store::Store;
 #[derive(Clone)]
 pub struct AppState {
     pub kafka_producer: Arc<rdkafka::producer::FutureProducer>,
     pub redis_pool: deadpool_redis::Pool,
     pub broadcaster: broadcast::Sender<String>,
-    pub kafka_topic: String
+    pub kafka_topic: String,
+    pub store: Arc<Store>,
 }
 
 #[tokio::main]
@@ -24,11 +27,15 @@ pub async fn main() -> Result<(),std::io::Error>{
     let consumer: StreamConsumer = init_consumer(&brokers,"rust-ws-service",&topic);
     let redis_pool = init_redis_pool().await;
     let (tx,_rx) = broadcast::channel(100);
+    let store = Arc::new(Store::new().map_err(|e|{
+        std::io::Error::new(std::io::ErrorKind::Other, format!("DB error {}", e))
+    })?);
     let state = AppState {
         kafka_producer:producer.clone(),
         kafka_topic:topic.clone(),
         redis_pool,
-        broadcaster:tx.clone()
+        broadcaster:tx.clone(),
+        store: store.clone(),
     };
 
     let bg_state = state.clone();
@@ -42,7 +49,9 @@ pub async fn main() -> Result<(),std::io::Error>{
         }
     });
 
-    let app = ws::ws_router().with_state(state);
+    let app = Router::new()
+    .merge(ws::ws_router())
+    .with_state(state);
     let addr = SocketAddr::from(([0,0,0,0],3000));
     info!("Server Listening on {}",addr);
     let listener = tokio::net::TcpListener::bind(&addr).await?;
